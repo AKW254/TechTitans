96e3b8f0a63cde9defc8380affac8d218a0a6c53
anatoli <anatoli@anatoli.anatoli>
2025-02-14 17:26:14 +0300

    Refactered Store Config to persists Changes

--- frontend/src/store/config.js
+++ frontend/src/store/config.js
@@ -1,13 +1,29 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import { thunk } from "redux-thunk";
-
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Uses LocalStorage
 import rootReducer from "./reducers";
 
+// Redux Persist Configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["posts"], // Persist only the posts reducer
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Redux DevTools Extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Create Redux Store
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+// Persistor
+export const persistor = persistStore(store);
 
 export default store;
--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import postReducer from "./postReducer";
 // import other reducers
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  post: postReducer,
   // ... other reducers
 });
 
