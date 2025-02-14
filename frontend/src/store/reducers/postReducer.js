240698616c00f7a0fc8023128e5290917bb91503
anatoli <anatoli@anatoli.anatoli>
2025-02-14 17:17:39 +0300

    Posts Reducer Is Implemented

--- frontend/src/store/reducers/postReducer.js
+++ frontend/src/store/reducers/postReducer.js
@@ -16,7 +16,7 @@ import {
   DELETE_POST_FAILURE,
 } from "../actionTypes";
 
-const initialState = JSON.parse(localStorage.getItem("postsState")) || {
+const initialState =  {
   posts: [],
   loading: false,
   error: null,
@@ -27,15 +27,42 @@ export const postReducer = (state = initialState, action) => {
     case GET_POSTS_REQUEST:
     case SINGLE_POST_REQUEST:
     case CREATE_POST_REQUEST:
+    case UPDATE_POST_REQUEST:    
     case DELETE_POST_REQUEST:
       return { ...state, loading: true, error: null };
 
     case GET_POSTS_SUCCESS:
-      return { ...state, posts: action.payload, loading: false, error: null };      
+      return { ...state, posts: action.payload, loading: false, error: null };
 
     case SINGLE_POST_SUCCESS:
       return { ...state, post: action.payload, loading: false, error: null };
 
     case CREATE_POST_SUCCESS:
-        return { ...state, posts: action.payload, loading: false, error: null };    
-     case    
+      return { ...state, posts: action.payload, loading: false, error: null };
+    case UPDATE_POST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        posts: state.posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        ),
+      };
+
+    case DELETE_POST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        posts: state.posts.filter((post) => post._id !== action.payload),
+      };
+    case GET_POSTS_FAILURE:
+    case SINGLE_POST_FAILURE:
+    case CREATE_POST_FAILURE:
+    case UPDATE_POST_FAILURE:
+    case DELETE_POST_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+     
